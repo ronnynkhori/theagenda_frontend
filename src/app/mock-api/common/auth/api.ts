4fc786c4961efdfa0abcf4004836ebc2d4fb866c
Ronnie Nkhori <RNkhori@bitri.co.bw>
2023-08-14 19:43:25 +0200

    save

--- src/app/mock-api/common/auth/api.ts
+++ src/app/mock-api/common/auth/api.ts
@@ -65,7 +65,7 @@ export class AuthMockApi
             .reply(({request}) =>
             {
                 // Sign in successful
-                if ( request.body.email === 'hughes.brian@company.com' && request.body.password === 'admin' )
+                if ( request.body.email === 'admin@mail.com' && request.body.password === 'admin' )
                 {
                     return [
                         200,
