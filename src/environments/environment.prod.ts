92d0dc02deb9bac32a4a05e663ef3261f548b034
Ronnie Nkhori <RNkhori@bitri.co.bw>
2024-03-20 10:19:15 +0200

    nginx

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
     name: 'prod',
     production: true,
-    apiBaseUrl: 'http://164.92.135.84:9090',
+    apiBaseUrl: 'http://nginx_proxy_manager',
     httpOptions:{
         headers: new HttpHeaders({'Content-Type': 'application/json'}),
         observe: 'response',
