a2fd0eee4f6ab35d587d0cb4cafaef96077eb320
Ronnie Nkhori <RNkhori@bitri.co.bw>
2024-04-04 18:43:29 +0200

    save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
     name: 'prod',
     production: true,
-    apiBaseUrl: 'http://backend:9090',
+    apiBaseUrl: 'http://159.223.194.235/:9090',
     httpOptions:{
         headers: new HttpHeaders({'Content-Type': 'application/json'}),
         observe: 'response',
