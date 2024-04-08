b133ef6b473f4d29d660d06c79e969a9cc2730ba
Ronnie Nkhori <RNkhori@bitri.co.bw>
2024-04-08 22:16:26 +0200

    save

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { HttpHeaders } from "@angular/common/http";
 export const environment = {
     name: 'prod',
     production: true,
-    apiBaseUrl: 'http://159.223.194.235/:9090',
+    apiBaseUrl: 'https://www.theagenda.co.bw:9090',
     httpOptions:{
         headers: new HttpHeaders({'Content-Type': 'application/json'}),
         observe: 'response',
